448ff63be0c660bb5b9438c7b54be46469dd249a
Abanoub Ghaly <abanoub.emil@almtar.com>
2021-04-20 18:36:32 +0200

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Abanoub Emil on 20/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
